a1611f665cc5e46840ed9ba19820fd2bf5a9b1a8
ehuntober <ehuntercrypto@gmail.com>
2023-01-27 16:47:40 +0000

    py_client

---
+++ py_client/basic.py
@@ -0,0 +1,10 @@
+import requests
+
+endpoint = "hhttps://httpbin.org/status/200"
+endpoint = "https://httpbin.org/anything"
+
+get_response =requests.get(endpoint, json={'queryt':
+                                          "hello world"}) # HTTP Request
+print(get_response.text) # print raw text code
+print(get_response.json())
+print(get_response.status_code)
