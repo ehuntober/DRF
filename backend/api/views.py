5e91ab4fd0901bd634598a7e06ae2aff33f4561e
ehuntober <ehuntercrypto@gmail.com>
2023-01-28 07:03:31 +0000

    py client modified

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+import json
 from django.http import JsonResponse
 
 def api_home(request,*args,**kwargs):
-    return JSONResponse({
+    body = request.body # byte string of JSON data
+    data ={}
+    try:
+        data = json.loads(body)
+    
+    except:
+        pass
+    print(data)
+    data['header'] = request.headers
+    
+    return JsonResponse({
         "message": "Hi there, this is your DJango API response"
     })
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,10 +2,10 @@ import requests
 
 # endpoint = "hhttps://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:7000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 
-get_response =requests.get(endpoint, json={'queryt':"hello world"}) # HTTP Request
-print(get_response.text) # print raw text code
-# print(get_response.json())
-print(get_response.status_code)
+get_response =requests.get(endpoint, params={'abc':123}, json={'query':"hello world"}) # HTTP Request
+#print(get_response.text) # print raw text code
+print(get_response.json())
+# print(get_response.status_code)
