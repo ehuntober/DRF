6cd01ba14f6db302bfae6b0ef95dccc812905b4b
ehuntober <ehuntercrypto@gmail.com>
2023-01-31 09:43:36 +0000

    views modified

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 #
 
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse\
+
+from products.models import Product
 
 def api_home(request,*args,**kwargs):
-    
+    model_data = Product.object.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['title']= model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        
+    return JsonResponse(data)
 
 
 
