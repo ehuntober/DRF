bdd5754c0405003a2078e274e92eceec9e588c4f
ehuntober <ehuntercrypto@gmail.com>
2023-01-31 23:35:56 +0000

    view added feature

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,17 +4,20 @@ from django.shortcuts import render
 #
 
 import json
-from django.http import JsonResponse\
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from products.models import Product
 
 def api_home(request,*args,**kwargs):
-    model_data = Product.object.all().order_by("?").first()
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title']= model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id','title','price'])
+        # data['id']= model_data.id
+        # data['title']= model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
         
     return JsonResponse(data)
 
