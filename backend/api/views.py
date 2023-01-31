409d2d00942c43e7a271133e981d9bcaabab6259
ehuntober <ehuntercrypto@gmail.com>
2023-01-31 09:32:10 +0000

    model create

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,17 +7,24 @@ import json
 from django.http import JsonResponse
 
 def api_home(request,*args,**kwargs):
-    body = request.body # byte string of JSON data
-    data ={}
-    try:
-        data = json.loads(body)
     
-    except:
-        pass
-    print(data)
-    data['header'] = request.headers
-    data['content_type'] = request.content_type
+
+
+
+
+
+# def api_home(request,*args,**kwargs):
+#     body = request.body # byte string of JSON data
+#     data ={}
+#     try:
+#         data = json.loads(body)
     
-    return JsonResponse({
-        "message": "Hi there, this is your DJango API response"
-    })
+#     except:
+#         pass
+#     print(data)
+#     data['header'] = request.headers
+#     data['content_type'] = request.content_type
+    
+#     return JsonResponse({
+#         "message": "Hi there, this is your DJango API response"
+#     })
