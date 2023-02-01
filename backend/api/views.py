e4f133ff0eb2a49718ca392a820329f730b12efe
ehuntober <ehuntercrypto@gmail.com>
2023-02-01 00:46:42 +0000

    view added

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,9 +7,17 @@ import json
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
+@api_view
 def api_home(request,*args,**kwargs):
+    
+    """
+    DRF API VIEW
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
@@ -19,7 +27,7 @@ def api_home(request,*args,**kwargs):
         # data['content'] = model_data.content
         # data['price'] = model_data.price
         
-    return JsonResponse(data)
+    return Response(data)
 
 
 
