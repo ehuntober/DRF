6fe7c02bc8cfc28f767f770241f3ad0a32d302de
ehuntober <ehuntercrypto@gmail.com>
2023-02-02 15:39:14 +0000

    model modified

--- backend/api/views.py
+++ backend/api/views.py
@@ -21,7 +21,7 @@ def api_home(request,*args,**kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price'])
+        data = model_to_dict(model_data, fields=['id','title','price', 'sale_price'])
         # data['id']= model_data.id
         # data['title']= model_data.title
         # data['content'] = model_data.content
