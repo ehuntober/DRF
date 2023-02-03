49e0f3959532cba460997e3ee31155e217b03bf0
ehuntober <ehuntercrypto@gmail.com>
2023-02-03 05:08:31 +0000

    serializers added

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,14 +1,21 @@
-from rest_framework import serializers
+
+from  rest_framework import serializers
 
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    # my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
-        fields  =[
+        fields  = [
             'title',
             'content',
             'price',
             'sale_price',
             'get_discount'
         ]
+        
+    def get_my_discount(self,obj):
+        print(obj.id)
+        
+        return obj.get_discount()
