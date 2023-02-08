5dc0023208ad4ec77a7fd2378209fd8efb520fe1
ehuntober <ehuntercrypto@gmail.com>
2023-02-08 11:28:21 +0000

    created nw data

--- py_client/create.py
+++ py_client/create.py
@@ -2,5 +2,9 @@ import requests
 
 endpoint = "http://localhost:8000/api/products/"
 
-get_response = request.post(endpoint)
+data = {
+    "title": "This field is done"
+}
+
+get_response = requests.post(endpoint,json=data)
 print(get_response.json())
