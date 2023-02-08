144c0ce38d8bd2db8e968915af100a830b5e411d
ehuntober <ehuntercrypto@gmail.com>
2023-02-08 15:24:37 +0000

    basic and list updated

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "hhttps://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/ap/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 
 get_response =requests.post(endpoint,json={'title':"hello world"}) # HTTP Request
