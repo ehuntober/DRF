bb293711673a1a7dbf9f9f8f0d7e0e20b36dcc9b
ehuntober <ehuntercrypto@gmail.com>
2023-02-08 11:50:28 +0000

    README file corrected

--- backend/products/views.py
+++ backend/products/views.py
@@ -12,11 +12,23 @@ class ProductCreateAPIView(generics.CreateAPIView):
     def perform_create(self,serializer):
         #serializer.save(user=self.request.user)
         print(serializer.validated_data)
-        serializer.save()
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = title
+        
+        serializer.save(content=content)
  
 product_create_view = ProductCreateAPIView.as_view() 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    
+    serializer_class = ProductSerializer
+    
+product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     
     serializer_class = ProductSerializer
