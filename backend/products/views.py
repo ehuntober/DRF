0d12c04a168c1cb744a521504449d8c414ee0c84
ehuntober <ehuntercrypto@gmail.com>
2023-02-08 14:51:14 +0000

    list added

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,7 +5,8 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+# class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
@@ -19,7 +20,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
         
         serializer.save(content=content)
  
-product_create_view = ProductCreateAPIView.as_view() 
+product_list_create_view = ProductListCreateAPIView.as_view() 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -28,10 +29,13 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
+# class ProductListAPIView(generics.ListAPIView):
+#     '''
+#       Not gonna use this met
+#     '''
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
     
-    serializer_class = ProductSerializer
-    
-product_detail_view = ProductDetailAPIView.as_view()
+# product_list_view = ProductListAPIView.as_view()
+
     
