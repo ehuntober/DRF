7e4fc9f8ed1be24cd80dd1b42f26ee569d6d0af0
ehuntober <ehuntercrypto@gmail.com>
2023-02-08 11:33:22 +0000

    views modified

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,6 +8,11 @@ from .serializers import ProductSerializer
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def perform_create(self,serializer):
+        #serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        serializer.save()
  
 product_create_view = ProductCreateAPIView.as_view() 
 
--- py_client/create.py
+++ py_client/create.py
@@ -3,7 +3,8 @@ import requests
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
-    "title": "This field is done"
+    "title": "This field is done",
+     "price" : 32.99
 }
 
 get_response = requests.post(endpoint,json=data)
